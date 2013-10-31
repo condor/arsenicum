7dd3f0f9f816b68f966442ed030bda1c57fdd018
condor <toyodan@r.recruit.co.jp>
2013-10-31 16:03:20 +0900

    Add comments that describe the methods to be implemented by the concrete classes.

--- lib/arsenicum/queue.rb
+++ lib/arsenicum/queue.rb
@@ -18,5 +18,32 @@ module Arsenicum
       json = JSON(hash.merge(timestamp: Time.to_f.to_s.tap{|t|t.sub! '.', ''}))
       put_to_queue(json)
     end
+
+    ######################################################
+    #
+    # Queue must implement the methods as below:
+    #   1. put_to_queue(json): putting the actual message
+    #     into the queue backend. The argument of this
+    #     method will be the JSON string.
+    #   2. poll: polling the queue and retrieve the
+    #     message information. This method is expected to
+    #     return the message Hash. Its keys and values
+    #     are expected as below:
+    #       :message_body: the raw string that was pushed
+    #         via the :put_to_queue method.
+    #       :message_id: the identifier of this message.
+    #         This is usually used to update the status of
+    #         message on the queue backend.
+    #   3. update_message_status(message_id, successful):
+    #     Update the status of the message. Arguments are
+    #     as following:
+    #       message_id: The identifier of the message to
+    #         be updated. This value will be set as the
+    #         :message_id of the return value of :poll.
+    #       successful: The result of the process done.
+    #         If the process complete successfully,
+    #         this argument will be set true. Otherwise,
+    #         this will be false.
+    #####################################################
   end
 end
