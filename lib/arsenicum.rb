a8828b6d77558deedb2f8b3fc3d8077743c4841d
condor <toyodan@r.recruit.co.jp>
2013-10-31 16:03:09 +0900

    Add autoload statements.

--- lib/arsenicum.rb
+++ lib/arsenicum.rb
@@ -1,2 +1,10 @@
-module Arsenicum; end
-require "arsenicum/version"
+module Arsenicum
+  autoload :Version,        'arsenicum/version'
+  autoload :Queue,          'arsenicum/queue'
+  autoload :Task,           'arsenicum/task'
+  autoload :Configuration,  'arsenicum/configuration'
+  autoload :Serialization,  'arsenicum/serialization'
+  autoload :WatchDog,       'arsenicum/watchdog'
+  autoload :QueueProxy,     'arsenicum/queue_proxy'
+  autoload :Syntax,         'arsenicum/syntax'
+end
