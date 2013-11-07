fd11b157bd7768f8847a986c7619007081033846
condor <toyodan@r.recruit.co.jp>
2013-11-07 12:36:33 +0900

    Syntax enabling process refined.

--- lib/arsenicum/syntax.rb
+++ lib/arsenicum/syntax.rb
@@ -3,7 +3,9 @@ module Arsenucum
     autoload :DelayedJob, 'arsenicum/syntax/delayed_job'
 
     def self.choose(syntax)
-      const_get syntax.to_s.gsub(/_([a-z])/){$1.upcase}.gsub(/^([a-z])/){$1.upcase}.to_sym
+      syntax_impl =
+        const_get syntax.to_s.gsub(/_([a-z])/){$1.upcase}.gsub(/^([a-z])/){$1.upcase}.to_sym
+      syntax_impl.enable!
     end
   end
 end
