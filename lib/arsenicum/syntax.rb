204bb35f9f9d3bfc9c0ab4451e70be2af5eb100c
condor <toyodan@r.recruit.co.jp>
2013-11-07 11:56:15 +0900

    Syntax chooser on Arsenicum::Syntax

--- lib/arsenicum/syntax.rb
+++ lib/arsenicum/syntax.rb
@@ -1,5 +1,9 @@
 module Arsenucum
   module Syntax
     autoload :DelayedJob, 'arsenicum/syntax/delayed_job'
+
+    def self.choose(syntax)
+      const_get syntax.to_s.gsub(/_([a-z])/){$1.upcase}.gsub(/^([a-z])/){$1.upcase}.to_sym
+    end
   end
 end
