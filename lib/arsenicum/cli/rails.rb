0deb9a7f132e783fa58c7f4104c73df754c5a52e
condor <toyodan@r.recruit.co.jp>
2013-11-07 12:37:38 +0900

    Fix cli options.

--- lib/arsenicum/cli/rails.rb
+++ lib/arsenicum/cli/rails.rb
@@ -2,12 +2,14 @@ module Arsenicum
   class CLI::Rails < CLI
     def boot
       ENV['RACK_ENV'] = ENV['RAILS_ENV'] = (configuration[:rails_env] || 'development')
-      require 'rails'
+      ENV['RAILS_ROOT'] ||= (configuration[:dir] || Dir.pwd)
+      load File.join(ENV['RAILS_ROOT'], 'config/environment.rb')
       super
     end
 
     def option_parser
-      super.register("-e", "--environment=ENVIRONMENT", -> v { {rails_env: v} })
+      super.register("-e", "--environment=ENVIRONMENT", -> v { {rails_env: v} }).
+        register("-d", "--dir=DIRECTORY", -> v { {dir: v} })
     end
   end
 end
