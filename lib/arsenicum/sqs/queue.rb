6af4f86622f9710164f3b642d6a6c1a421125d31
condor <toyodan@r.recruit.co.jp>
2013-11-07 11:51:55 +0900

    Add queue creation.

--- lib/arsenicum/sqs/queue.rb
+++ lib/arsenicum/sqs/queue.rb
@@ -6,17 +6,20 @@ module Arsenicum::Sqs
     attr_reader :sqs
     attr_reader :wait_timeout
     attr_reader :failure_queue_name
+    attr_reader :queue_configuration
 
     def configure(config)
-      @account = config[:account]
+      @account = config.delete :account
       @sqs = AWS::SQS.new account
       @wait_timeout =
-        if config[:long_poll]
+        if config.delete(:long_poll)
           nil
-        elsif config[:wait_timeout]
-          config[:wait_timeout].to_i
+        elsif timeout = config.delete(:wait_timeout)
+          timeout.to_i
         end
-      @failure_queue_name = config[:failure_queue_name]
+      @failure_queue_name = config.delete :failure_queue_name
+
+      @queue_configuration = config
     end
 
     def put_to_queue(json, named: name)
@@ -38,6 +41,35 @@ module Arsenicum::Sqs
 
       sqs_queue = sqs.named(name)
       sqs.client.delete_message queue_url: sqs_queue.url, receipt_handle: message_id
+    end
+
+    CREATION_OPTIONS = [
+      :visibility_timeout, :maximum_message_size,
+      :delay_seconds, :message_retention_period,
+    ].freeze
+    CREATION_OPTIONS_IN_JSON = [
+      :policy,
+    ].freeze
+
+    def create_queue_backend
+      creation_options = queue_configuration.values_at(*CREATION_OPTIONS).
+        each_with_index.inject({}) do |opts, vi|
+
+        (value, i) = vi
+        opts[CREATION_OPTIONS[i]] = value if value
+        opts
+      end
+      CREATION_OPTIONS_IN_JSON.each do |opt|
+        creation_options[opt] = JSON(queue_configuration[opt]) if queue_configuration[opt]
+      end
+
+      begin
+        sqs.queues.named(name.to_s)
+        puts "Not Created Queue #{name}:Exists"
+      rescue AWS::SQS::Errors::NonExistentQueue
+        sqs.queues.create name.to_s, creation_options
+        puts "Created Queue #{name}"
+      end
     end
   end
 end
