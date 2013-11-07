36221d502a25b98e03b83601a23864f09b727699
condor <toyodan@r.recruit.co.jp>
2013-11-07 22:14:08 +0900

    Fix CLI option passing.

--- lib/arsenicum/cli/rails.rb
+++ lib/arsenicum/cli/rails.rb
@@ -1,9 +1,16 @@
 module Arsenicum
   class CLI::Rails < CLI
     def boot
-      ENV['RACK_ENV'] = ENV['RAILS_ENV'] = (configuration[:rails_env] || 'development')
-      ENV['RAILS_ROOT'] ||= (configuration[:dir] || Dir.pwd)
-      load File.join(ENV['RAILS_ROOT'], 'config/environment.rb')
+      ENV['RACK_ENV'] = (ENV['RAILS_ENV'] ||= (configuration[:rails_env] || 'development'))
+      rootdir = ENV['RAILS_ROOT'] || configuration[:dir] || Dir.pwd
+      Dir.chdir rootdir
+
+      if configuration[:background] && !configuration[:pidfile]
+        configuration[:pidfile] = "#{rootdir}/tmp/pids/arsenicum.pid"
+      end
+
+      load File.join(rootdir, 'config/environment.rb')
+      Arsenicum::Configuration.reconfigure configuration
       Arsenicum::Server.start
     end
 
@@ -11,7 +18,7 @@ module Arsenicum
       OptionParser.new.register("-e", "--environment=ENVIRONMENT", -> v { {rails_env: v} }).
         register("-d", "--dir=DIRECTORY", -> v { {dir: v} }).
         register("-p", "--pidfile=PID_FILE", -> v { { pidfile: v } }).
-        register("-l", "--log-file=LOG_FILE", -> v { { log_file: v } })
+        register("-l", "--log-file=LOG_FILE", -> v { { log_file: v } }).
         register("-D", "--daemon", -> v { { background: true } })
     end
   end
