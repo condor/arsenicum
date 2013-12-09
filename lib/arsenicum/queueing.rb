61884726b806eacd7da0f6fe1cf8ea810c2bc30c
condor <toyodan@r.recruit.co.jp>
2013-12-09 21:06:47 +0900

    Add PostOffice to the autoload target.

--- lib/arsenicum/queueing.rb
+++ lib/arsenicum/queueing.rb
@@ -2,5 +2,6 @@ module Arsenicum
   module Queueing
     autoload :Request,    'arsenicum/queueing/request'
     autoload :Serializer, 'arsenicum/queueing/serializer'
+    autoload :PostOffice, 'arsenicum/queueing/post_office'
   end
 end
