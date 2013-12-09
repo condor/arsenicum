388434c5a304ba09d38d7d4a38201a5cfa0e0318
condor <toyodan@r.recruit.co.jp>
2013-12-09 13:18:32 +0900

    Fix autoload / active_record loading.

--- lib/arsenicum.rb
+++ lib/arsenicum.rb
@@ -1,14 +1,16 @@
 module Arsenicum
   autoload :Version,        'arsenicum/version'
   autoload :Queue,          'arsenicum/queue'
-  autoload :Task,           'arsenicum/task'
   autoload :Configuration,  'arsenicum/configuration'
-  autoload :Serialization,  'arsenicum/serialization'
-  autoload :WatchDog,       'arsenicum/watchdog'
-  autoload :QueueProxy,     'arsenicum/queue_proxy'
   autoload :Syntax,         'arsenicum/syntax'
   autoload :Sqs,            'arsenicum/sqs'
   autoload :CLI,            'arsenicum/cli'
   autoload :Server,         'arsenicum/server'
-  autoload :Actor,          'arsenicum/actor'
+  autoload :Queueing,       'arsenicum/queueing'
+end
+
+if defined? ::ActiveSupport
+  ActiveSupport.on_load :active_record do
+    Arsenicum::Queueing::Serializer.send :include, Arsenicum::Queueing::Serializer::WithActiveRecord
+  end
 end
