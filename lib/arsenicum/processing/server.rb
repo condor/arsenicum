f4d4dc7ad994a1a77f7be8e2df959959a30c4ae4
condor <toyodan@r.recruit.co.jp>
2013-12-09 22:29:05 +0900

    Booting process refined.

--- lib/arsenicum/processing/server.rb
+++ lib/arsenicum/processing/server.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module Arsenicum
   module Processing
     class Server
@@ -10,12 +12,16 @@ module Arsenicum
 
       def self.start(config = Arsenicum::Configuration.instance)
         Process.daemon(true, true) if config.server.background
-        File.open(config.pidfile, 'w'){|f|f.puts $$} if config.server.pidfile
+        if config.server.pidfile
+          dirname = File.dirname(config.server.pidfile)
+          FileUtils.mkpath dirname unless Dir.exist? dirname
+          File.open(config.server.pidfile, 'w'){|f|f.puts $$}
+        end
         new(config).start
       end
 
       def start
-        puts "Booting Arsenicum Server..."
+        config.logger.info "Booting Arsenicum Processor..."
         Signal.trap(:INT, &method(:trap_interruption))
 
         @queue_pickers = config.queue_configurations.map do |kv|
@@ -35,8 +41,11 @@ module Arsenicum
       end
 
       def shutdown
+        puts "Shutting down Arsenicum Processor..."
+
         @queue_pickers.each(&:shutdown)
         File.delete config.server.pidfile if config.server.pidfile && File.exist?(config.server.pidfile)
+        puts "Shutting down completed"
         Thread.current.terminate
       end
 
