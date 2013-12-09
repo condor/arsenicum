816d58e6495378139aba8579a603c24bd0a781df
condor <toyodan@r.recruit.co.jp>
2013-12-09 12:33:34 +0900

    Restructured.

--- lib/arsenicum/queueing/request.rb
+++ lib/arsenicum/queueing/request.rb
@@ -2,55 +2,54 @@ require 'json'
 
 module Arsenicum
   module Queueing
+    class Request
+      include Serialization
 
-  end
-  class Request
-    include Serialization
+      attr_reader :target, :method, :arguments, :timestamp, :message_id, :exception
 
-    attr_reader :target, :method, :arguments, :timestamp, :message_id, :exception
+      def self.parse(raw_message, message_id)
+        message_content = JSON(raw_message)
 
-    def self.parse(raw_message, message_id)
-      message_content = JSON(raw_message)
+        timestamp = message_content['timestamp']
+        method = message_content['method_name'].to_sym
 
-      timestamp = message_content['timestamp']
-      method = message_content['method_name'].to_sym
+        target = restore(message_content['target'])
+        arguments = message_content['arguments'].nil? ? [] :
+            message_content['arguments'].map{|arg|restore(arg)}
 
-      target = restore(message_content['target'])
-      arguments = message_content['arguments'].nil? ? [] :
-        message_content['arguments'].map{|arg|restore(arg)}
+        new(target, method, arguments, timestamp, message_id)
+      end
 
-      new(target, method, arguments, timestamp, message_id)
-    end
+      def initialize(target, method, arguments, timestamp, message_id)
+        @target     = target
+        @method     = method
+        @arguments  = arguments
+        @timestamp  = timestamp
+        @message_id = message_id
+      end
 
-    def initialize(target, method, arguments, timestamp, message_id)
-      @target     = target
-      @method     = method
-      @arguments  = arguments
-      @timestamp  = timestamp
-      @message_id = message_id
-    end
+      def prepare_serialization
+        {
+            target: prepare_serialization(target),
+            timestamp: (Time.now.to_f * 1000000).to_i,
+            method_name: method_name,
+            arguments: arguments.nil? ? nil : arguments.map{|arg|prepare_serialization(arg)},
+        }
+      end
 
-    def prepare_serialization
-      {
-        target: prepare_serialization(target),
-        timestamp: (Time.now.to_f * 1000000).to_i,
-        method_name: method_name,
-        arguments: arguments.nil? ? nil : arguments.map{|arg|prepare_serialization(arg)},
-      }
-    end
+      def serialize
+        JSON(prepare_serialization)
+      end
 
-    def serialize
-      JSON(prepare_serialization)
-    end
+      def execute!
+        target.__send__ method, *arguments
+      rescue Exception => e
+        @exception = e
+      end
 
-    def execute!
-      target.__send__ method, *arguments
-    rescue Exception => e
-      @exception = e
-    end
-
-    def successful?
-      !exception
+      def successful?
+        !exception
+      end
     end
   end
 end
