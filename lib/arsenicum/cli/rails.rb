33d40aa77f92c560241bfca124941a124c360682
condor <toyodan@r.recruit.co.jp>
2013-12-09 22:18:57 +0900

    Argument processing refined.

--- lib/arsenicum/cli/rails.rb
+++ lib/arsenicum/cli/rails.rb
@@ -4,18 +4,20 @@ module Arsenicum
 
     def boot
       configuration = self.configuration
-      server_config = configuration.delete(:server)
+      environment = ENV['RAILS_ENV'] || :development
+      environment = environment.to_sym
+      config_for_env = configuration[environment]
+      raise Arsenicum::MisconfigurationError, "config for #{environment} is abscent" unless config_for_env
 
-      rails_env = server_config.delete(:environment) || ENV['RAILS_ENV'] || :development
-      rails_env = rails_env.to_sym
+      server_config = config_for_env.delete(:server) || {}
+      server_config.merge!(configuration.delete(:server) || {})
+
       dir = server_config.delete(:working_directory) || ENV['RAILS_ROOT'] || Dir.pwd
       dir = File.expand_path dir
       background = server_config.delete(:background)
       pidfile = server_config.delete(:pidfile) || "#{dir}/tmp/pids/arsenicum.pid" if background
 
       Dir.chdir dir
-      config_for_env = server_config[rails_env]
-      raise Arsenicum::MisconfigurationError, "config for #{rails_env} is abscent" unless config_for_env
 
       config_for_env.merge!(
           server: {
@@ -26,7 +28,6 @@ module Arsenicum
           },
       )
 
-      ENV['RAILS_ENV'] = rails_env.to_s
       load File.join(dir, 'config/environment.rb')
       Arsenicum::Configuration.configure config_for_env
       Arsenicum::Processing::Server.start
@@ -35,8 +36,8 @@ module Arsenicum
     private
     def option_parser
       OptionParser.new.
-        register("-e", "--environment=ENV", -> v {{rails_env: v}}).
-        register("-d", "--rails-root=DIR", -> v {{working_directory: v}}).
+        register("-e", "--environment=ENV", -> v, _ {ENV['RAILS_ENV'] = v.to_s}).
+        register("-d", "--rails-root=DIR", -> v, h {h[:server]||={};h[:server].merge!(working_directory: v)}).
         register("-c", "--config-file=FILE",
                  -> v {v.end_with?(".yml") ? YAML.load(File.read v, encoding: 'UTF-8') : load(v)})
     end
