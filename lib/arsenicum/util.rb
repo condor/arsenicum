a7ed0bff3950abd6391145aa5c9344d960edcab3
condor <toyodan@r.recruit.co.jp>
2013-12-09 20:16:07 +0900

    Add Util methods.

---
+++ lib/arsenicum/util.rb
@@ -0,0 +1,18 @@
+module Arsenicum
+  module Util
+    def normalize_hash(values)
+      values.inject({}) do |h, kv|
+        (key, value) = kv
+        value = normalize_hash(values) if value.is_a? Hash
+        h.tap{|i|i.merge!(key.to_sym => value)}
+      end
+    end
+
+    def camelcase(stringlike, upcase_first = true)
+      stringlike.to_s.tap do |s|
+        s.gsub!(/_([a-z])/){$1.upcase}
+        s.gsub!(/^([a-z])/){$1.upcase} if upcase_first
+      end.to_sym
+    end
+  end
+end
