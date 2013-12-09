a8ccdf796d4bcc43e9f4e503fcc99d79b1868ec6
condor <toyodan@r.recruit.co.jp>
2013-12-09 13:16:37 +0900

    Fix API change.

--- lib/arsenicum/syntax/delayed_job.rb
+++ lib/arsenicum/syntax/delayed_job.rb
@@ -6,7 +6,7 @@ module Arsenicum::Syntax
       end
 
       def method_missing(method_id, *arguments)
-        ::Arsenicum::QueueProxy.instance.async(@wrapped_object, method_id, *arguments)
+        ::Arsenicum::Configuration.instance.post_office.deliver_to(@wrapped_object, method_id, *arguments)
       end
     end
 
