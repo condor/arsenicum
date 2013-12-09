c169362136da5239ac5f8688f6354abae685e866
condor <toyodan@r.recruit.co.jp>
2013-12-09 14:02:59 +0900

    Restructured.

--- lib/arsenicum/processing/server.rb
+++ lib/arsenicum/processing/server.rb
@@ -1,42 +1,41 @@
 module Arsenicum
-  class Server
-    autoload :WatchDog, 'arsenicum/server/watchdog'
-    autoload :Actor, 'arsenicum/server/actor'
+  module Processing
+    class Server
+      attr_reader :watchdogs
+      attr_reader :config
 
-    attr_reader :watchdogs
-    attr_reader :config
-
-    def initialize(config)
-      @config = config
-    end
-
-    def self.start(config = Arsenicum::Configuration.instance)
-      Process.daemon(true, true) if config.background
-      File.open(config.pidfile, 'w'){|f|f.puts $$} if config.pidfile
-      new(config).start
-    end
-
-    def start
-      puts "Booting Arsenicum Server..."
-      Signal.trap(:INT, &method(:trap_interruption))
-
-      @watchdogs = config.create_queues.map do |queue|
-        Arsenicum::WatchDog.new(queue, config.logger)
+      def initialize(config)
+        @config = config
       end
-      @watchdogs.each(&:boot)
 
-      loop { sleep 10 }
-    end
+      def self.start(config = Arsenicum::Configuration.instance)
+        Process.daemon(true, true) if config.background
+        File.open(config.pidfile, 'w'){|f|f.puts $$} if config.pidfile
+        new(config).start
+      end
 
-    def shutdown
-      @watchdogs.each(&:shutdown)
-      File.delete config.pidfile if config.pidfile
-      Thread.current.terminate
-    end
+      def start
+        puts "Booting Arsenicum Server..."
+        Signal.trap(:INT, &method(:trap_interruption))
 
-    private
-    def trap_interruption(*)
-      shutdown
+        @watchdogs = config.create_queues.map do |queue|
+          Arsenicum::WatchDog.new(queue, config.logger)
+        end
+        @watchdogs.each(&:boot)
+
+        loop { sleep 10 }
+      end
+
+      def shutdown
+        @watchdogs.each(&:shutdown)
+        File.delete config.pidfile if config.pidfile
+        Thread.current.terminate
+      end
+
+      private
+      def trap_interruption(*)
+        shutdown
+      end
     end
   end
 end
