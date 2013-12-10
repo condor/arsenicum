18d6f490ae85bd685415920431b6481e0588ea80
condor <toyodan@r.recruit.co.jp>
2013-12-10 19:38:40 +0900

    Add :underscore.

--- lib/arsenicum/util.rb
+++ lib/arsenicum/util.rb
@@ -1,5 +1,10 @@
 module Arsenicum
   module Util
+    def self.included(base)
+      mod = self
+      base.module_eval{extend mod}
+    end
+
     def normalize_hash(values)
       values.inject({}) do |h, kv|
         (key, value) = kv
@@ -9,10 +14,19 @@ module Arsenicum
     end
 
     def camelcase(stringlike, upcase_first = true)
-      stringlike.to_s.tap do |s|
+      stringlike.to_s.dup.tap do |s|
         s.gsub!(/_([a-z])/){$1.upcase}
         s.gsub!(/^([a-z])/){$1.upcase} if upcase_first
       end.to_sym
     end
+
+    def underscore(stringlike)
+      stringlike.to_s.dup.tap do |s|
+        s.gsub!(/^([A-Z])/){$1.tap(&:downcase!)}
+        s.gsub!(/([A-Z])/){'_' << $1.tap(&:downcase!)}
+      end
+    end
+
+    extend self
   end
 end
