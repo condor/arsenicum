2d8415f7cf8c66ab37c28b69d51596779f1647de
condor <toyodan@r.recruit.co.jp>
2013-12-10 12:52:56 +0900

    serialize_restore is applicable to most of the cases.

--- spec/arsenicum/queueing/serializer_spec.rb
+++ spec/arsenicum/queueing/serializer_spec.rb
@@ -7,10 +7,14 @@ describe Arsenicum::Queueing::Serializer do
 
   describe [:serialize_object, :restore_object] do
 
+    shared_examples_for :serialize_restore do
+      specify{expect(subject.restore_object subject.serialize_object target).to eq(target)}
+    end
+
     context :target_is_raw do
       shared_examples_for :raw_object do
         specify{expect(subject.serialize_object target).to eq({type: 'raw', value: target.inspect})}
-        specify{expect(subject.restore_object subject.serialize_object target).to eq(target)}
+        it_should_behave_like :serialize_restore
       end
 
       context :target_is_integer do
