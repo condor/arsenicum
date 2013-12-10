386c2977c70a6f18284cc3b0b26da68ff17ab57e
condor <toyodan@r.recruit.co.jp>
2013-12-10 12:51:12 +0900

    Fix serializer to use String type symbols.

--- lib/arsenicum/queueing/serializer.rb
+++ lib/arsenicum/queueing/serializer.rb
@@ -7,40 +7,50 @@ module Arsenicum
       DATE_FORMAT = "%Y-%m-%d".freeze
       DATE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S %Z %z".freeze
 
+      include Arsenicum::Util
+
       def serialize_object(value)
         serialize_object_specific(value) || serialize_object_default(value)
       end
+
+      TYPE_RAW      = 'raw'.freeze
+      TYPE_DATE     = 'date'.freeze
+      TYPE_DATETIME = 'time'.freeze
+      TYPE_CLASS    = 'class'.freeze
+      TYPE_ARRAY    = 'array'.freeze
+      TYPE_HASH     = 'hash'.freeze
+      TYPE_ANY      = 'marshal'.freeze
 
       def serialize_object_specific(value)
         case value
           when Integer, Float, String, TrueClass, FalseClass, NilClass
             {
-                type: :raw,
+                type: TYPE_RAW,
                 value: value.inspect,
             }
           when Date
             {
-                type: 'date',
+                type: TYPE_DATE,
                 value: value.strftime(DATE_FORMAT),
             }
           when DateTime, Time
             {
-                type: 'time',
+                type: TYPE_DATETIME,
                 value: value.strftime(DATE_TIME_FORMAT),
             }
           when Class
             {
-                type: :class,
+                type: TYPE_CLASS,
                 value: value.name,
             }
           when Array
             {
-                type: :array,
+                type: TYPE_ARRAY,
                 values: value.map{|v|serialize_object(v)},
             }
           when Hash
             {
-                type: :hash,
+                type: TYPE_HASH,
                 values: value.inject({}){|h, kv|(k,v)=kv;h[k.to_s]=serialize_object(v);h},
             }
         end
@@ -48,30 +58,32 @@ module Arsenicum
 
       def serialize_object_default(value)
         {
-            type: 'marshal',
+            type: TYPE_ANY,
             value: Marshal.dump(value).unpack('H*').first,
         }
       end
 
       def restore_object(value)
-        return eval value['value'] if value['type'] == 'raw'
+        value = normalize_hash(value)
+
+        return eval value[:value] if value[:type] == TYPE_RAW
         restore_object_specific(value) || restore_object_default(value)
       end
 
       def restore_object_specific(value)
-        case value['type']
-          when 'date'
-            Date.strptime(value['value'], DATE_FORMAT)
-          when 'time'
-            Time.strptime(value['value'], DATE_TIME_FORMAT)
-          when 'class'
-            Module.const_get value['value'].to_sym
-          when 'array'
-            value['values'].map do |value|
+        case value[:type]
+          when TYPE_DATE
+            Date.strptime(value[:value], DATE_FORMAT)
+          when TYPE_DATETIME
+            Time.strptime(value[:value], DATE_TIME_FORMAT)
+          when TYPE_CLASS
+            Module.const_get value[:value].to_sym
+          when TYPE_ARRAY
+            value[:values].map do |value|
               restore_object(value)
             end
-          when 'hash'
-            value['values'].inject({}) do |h, key_value|
+          when TYPE_HASH
+            value[:values].inject({}) do |h, key_value|
               (key, value) = key_value
               h[key.to_sym] = restore_object(key_value)
               h
@@ -80,10 +92,12 @@ module Arsenicum
       end
 
       def restore_object_default(value)
-        Marshal.restore_object [value['value']].pack('H*')
+        Marshal.restore_object [value[:value]].pack('H*')
       end
 
       module WithActiveRecord
+        TYPE_ACTIVE_RECORD = 'active_record'.freeze
+
         def self.included(base)
           base.module_eval do
             def serialize_object_specific_with_active_record(value)
@@ -97,15 +111,15 @@ module Arsenicum
             private
             def serialize_object_active_record(value)
               return {
-                  type: :active_record,
+                  type: TYPE_ACTIVE_RECORD,
                   class: value.class.name,
                   id: value.id,
               } if value.is_a? ActiveRecord::Base
             end
 
             def restore_object_active_record(value)
-              if value['class'] == 'active_record'
-                klass = const_get value['class'].to_sym
+              if value[TYPE_CLASS] == TYPE_ACTIVE_RECORD
+                klass = const_get value[TYPE_CLASS].to_sym
                 klass.find value['id']
               end
             end
