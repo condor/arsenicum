c731ae6d83d3d0aa4218b1d3f020e397517b09e1
condor <toyodan@r.recruit.co.jp>
2014-11-20 17:51:27 +0900

    Fix env var operation.

--- lib/arsenicum/main/rails_main.rb
+++ lib/arsenicum/main/rails_main.rb
@@ -1,7 +1,7 @@
 class Arsenicum::Main::RailsMain < Arsenicum::Main
   def before_boot(config)
     env = config.rails_env || 'development'
-    ENV['RAILS_ENV'] = env
+    ENV['RAILS_ENV'] = env.to_s
 
     require './config/environment'
   end
