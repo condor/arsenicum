5774e8404c3fed3b0253ec64963f67032bea24cf
condor <condor1226@gmail.com>
2014-12-30 08:02:10 +0900

    Main refactored

--- lib/arsenicum/main.rb
+++ lib/arsenicum/main.rb
@@ -1,38 +1,67 @@
 module Arsenicum
   class Main
-    attr_reader :queues
+    attr_reader :config
 
-    def run(config)
-      $0 = 'arsenicum[main]'
-
-      if config.daemon
-        Process.daemon  true, true
-
-        File.open(config.pidfile_path, 'w:UTF-8') do |f|
-          f.puts $$
-        end
+    class << self
+      def run(config, main_class: Arsenicum::Main)
+        main = main_class.new config
+        main.run
       end
 
-      Dir.chdir     config.working_directory
-
-      configure_io  config
-      configure_log config
-
-      before_boot(config)
-
-      @queues = config.queue_configurations.map{|qc|qc.build}
-      threads = @queues.map(&:start_async)
-      begin
-        sleep 10
-        trap_signal
-
-        threads.each(&:join)
-      rescue Interrupt
+      private
+      def attr_config_reader(*attr_names)
+        attr_names.each do |attr_name|
+          class_eval <<-SCRIPT, __FILE__, __LINE__ + 1
+          def #{attr_name}
+            config.#{attr_name}
+          end
+          SCRIPT
+        end
       end
     end
 
+    def initialize(config)
+      @config = config
+      configure
+    end
+
+    def run(config)
+      rename_process
+      trap_signal
+      daemonize_if_required
+      move_directory
+
+      configure_io
+      configure_log
+
+      before_boot
+      boot
+
+      waiting_loop
+    end
+
     private
-    def configure_io(config)
+    def configure;end
+
+    def rename_process
+      $0 = "#{process_name}[main]"
+    end
+
+    def move_directory
+      Dir.chdir     config.working_directory
+    end
+
+    def daemonize_if_required
+      return unless config.daemon
+
+      Process.daemon  true, true
+
+      File.open(config.pidfile_path, 'w:UTF-8') do |f|
+        f.puts $$
+      end
+    end
+
+    def configure_io
       $stdout = File.open(config.stdout_path, 'a:UTF-8') if config.stdout_path
 
       if config.stderr_path
@@ -44,22 +73,33 @@ module Arsenicum
       end
     end
 
-    def before_boot(config);end
-
-    def configure_log(config)
+    def configure_log
       Arsenicum::Logger.set_logger config.logger_config.build
     end
+
+    def before_boot;end
 
     def trap_signal
       [:TERM, :INT,].each do |sig|
         Signal.trap sig do
-puts "signal received"
           queues.each(&:stop)
           exit 1
         end
       end
     end
 
+    def process_name
+      'arsenicum'
+    end
+
+    def waiting_loop
+      begin
+        loop{sleep 100}
+      rescue Interrupt
+      end
+    end
+
     autoload  :RailsMain, 'arsenicum/main/rails_main'
+    autoload  :Queue,     'arsenicum/main/queue'
   end
 end
